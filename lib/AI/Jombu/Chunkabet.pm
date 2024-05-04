a9f0161ad3a20c7cae73922e4f734dccd0a6a1f2
Michael Roberts <michael@vivtek.com>
2024-05-03 20:06:56 -0400

    Rationalized the chunkabet a little

--- lib/AI/Jombu/Chunkabet.pm
+++ lib/AI/Jombu/Chunkabet.pm
@@ -19,132 +19,325 @@ Version 0.01
 our $VERSION = '0.01';
 
 our %chunkabet = (
-   "a-final"       => "wow",
    "a-initial"     => "wow",
    "a-middle"      => "wow",
+   "a-final"       => "wow",
+   "a"             => "wow",
+   
    "ai-initial"    => "meh",
    "ai-middle"     => "good",
+   "ai"            => "good",
+   
    "au-middle"     => "meh",
-   "ay-final"      => "good",
+   "au"            => "good",
+   
    "ay-middle"     => "meh",
+   "ay-final"      => "good",
+   "ay"            => "good",
+   
    "b-initial"     => "wow",
+   "b"             => "wow",
+   
    "bl-initial"    => "ok",
-   "c-final"       => "ok",
+   "bl"            => "ok",
+   
+   "ble-final"     => "ok", # Added manually 2024-05-02
+   "ble"           => "ok",
+
+   "br-initial"    => "ok", # Added manually 2024-05-02
+   "br"            => "ok",
+  
    "c-initial"     => "strong",
+   "c-final"       => "ok",
+   "c"             => "strong",
+   
    "ce-final"      => "meh",
+   "ce"            => "meh",
+   
+   "ch-initial"    => "wow", # Added manually 2024-05-02
    "ch-final"      => "good",
+   "ch"            => "wow",
+   
    "ck-final"      => "ok",
-   "d-final"       => "strong",
+   "ck"            => "ok",
+   
    "d-initial"     => "strong",
+   "d-final"       => "strong",
+   "d"             => "d",
+   
    "dd-final"      => "meh",
+   "dd"            => "meh",
+   
    "de-final"      => "ok",
-   "e-final"       => "wow",
+   "de"            => "ok",
+   
    "e-initial"     => "wow",
    "e-middle"      => "wow",
+   "e-final"       => "wow",
+   "e"             => "wow",
+
    "ea-initial"    => "good",
    "ea-middle"     => "good",
-   "ee-final"      => "ok",
+   "ea"            => "good",
+   
    "ee-middle"     => "good",
+   "ee-final"      => "ok",
+   "ee"            => "good",
+   
    "ei-middle"     => "ok",
+   "ei"            => "ok",
+   
    "eo-final"      => "ok",
+   "eo"            => "ok",
+   
    "ey-final"      => "good",
-   "f-final"       => "wow",
+   "ey"            => "ok",
+   
    "f-initial"     => "strong",
+   "f-final"       => "wow",
+   "f"             => "wow",
+   
    "ff-final"      => "meh",
+   "ff"            => "meh",
+   
    "fr-initial"    => "good",
-   "g-final"       => "meh",
+   "fr"            => "good",
+   
    "g-initial"     => "strong",
+   "g-final"       => "meh",
+   "g"             => "strong",
+   
    "gh-final"      => "ok",
+   "gh"            => "ok",
+   
    "ght-final"     => "ok",
+   "ght"           => "ok",
+   
    "gr-initial"    => "meh",
+   "gr"            => "meh",
+   
    "h-initial"     => "wow",
-   "i-final"       => "strong",
+   "h"             => "wow",
+   
    "i-initial"     => "wow",
    "i-middle"      => "wow",
+   "i-final"       => "strong",
+   "i"             => "wow",
+   
    "ie-final"      => "ok",
+   "ie"            => "ok",
+   
    "io-middle"     => "good",
+   "io"            => "good",
+   
    "j-initial"     => "ok",
+   "j"             => "ok",
+   
    "k-final"       => "ok",
+   "k"             => "ok",
+   
    "ke-final"      => "good",
+   "ke"            => "good",
+   
    "kn-initial"    => "meh",
-   "l-final"       => "strong",
+   "kn"            => "meh",
+   
    "l-initial"     => "wow",
+   "l-final"       => "strong",
+   "l"             => "wow",
+   
    "ld-final"      => "good",
+   "ld"            => "good",
+   
    "lf-final"      => "meh",
+   "lf"            => "meh",
+   
    "ll-final"      => "strong",
+   "ll"            => "strong",
+   
    "lp-final"      => "meh",
-   "m-final"       => "strong",
+   "lp"            => "meh",
+
    "m-initial"     => "wow",
+   "m-final"       => "strong",
+   "m"             => "wow",
+   
    "me-final"      => "strong",
-   "n-final"       => "wow",
+   "me"            => "strong",
+   
    "n-initial"     => "strong",
+   "n-final"       => "wow",
+   "n"             => "wow",
+   
    "nce-final"     => "meh",
+   "nce"           => "meh",
+
    "nd-final"      => "wow",
+   "nd"            => "wow",
+
    "ne-final"      => "good",
+   "ne"            => "good",
+
    "ng-final"      => "strong",
+   "ng"            => "strong",
+
    "ngs-final"     => "meh",
+   "ngs"           => "meh",
+   
    "nk-final"      => "meh",
+   "nk"            => "meh",
+
    "ns-final"      => "ok",
+   "ns"            => "ok",
+
    "nt-final"      => "good",
-   "o-final"       => "wow",
+   "nt"            => "good",
+   
    "o-initial"     => "wow",
-   "oa-middle"     => "wow", # added manually 2024-04-01
    "o-middle"      => "wow",
+   "o-final"       => "wow",
+   "o"             => "wow",
+   
+   "oa-middle"     => "wow", # added manually 2024-04-01
+   "oa"            => "wow",
+   
    "oe-middle"     => "meh",
-   "oo-final"      => "meh",
+   "oe"            => "meh",
+   
    "oo-middle"     => "good",
+   "oo-final"      => "meh",
+   "oo"            => "good",
+   
    "ou-initial"    => "good",
    "ou-middle"     => "strong",
-   "p-final"       => "good",
+   "ou"            => "strong",
+   
    "p-initial"     => "strong",
+   "p-final"       => "good",
+   "p"             => "strong",
+   
    "pl-initial"    => "good",
+   "pl"            => "good",
+   
    "pr-initial"    => "ok",
-   "r-final"       => "wow",
+   "pr"            => "ok",
+   
    "r-initial"     => "strong",
+   "r-final"       => "wow",
+   "r"             => "wow",
+   
    "rd-final"      => "ok",
+   "rd"            => "ok",
+
    "rds-final"     => "ok",
+   "rds"           => "ok",
+
    "re-final"      => "strong",
+   "re"            => "strong",
+
    "rk-final"      => "meh",
+   "rk"            => "meh",
+
    "rld-final"     => "meh",
+   "rld"           => "meh",
+
    "rm-final"      => "meh",
+   "rm"            => "meh",
+
    "rn-final"      => "meh",
+   "rn"            => "meh",
+
    "rs-final"      => "good",
+   "rs"            => "good",
+
    "rst-final"     => "ok",
+   "rst"           => "ok",
+
    "rt-final"      => "meh",
+   "rt"            => "meh",
+
    "rth-final"     => "meh",
-   "s-final"       => "wow",
+   "rth"           => "meh",
+
    "s-initial"     => "strong",
+   "s-final"       => "wow",
+   "s"             => "wow",
+
    "se-final"      => "good",
+   "se"            => "good",
+
    "sh-initial"    => "good",
+   "sh"            => "good",
+
    "sm-initial"    => "meh",
-   "st-final"      => "good",
+   "sm"            => "meh",
+
    "st-initial"    => "ok",
-   "t-final"       => "wow",
+   "st-final"      => "good",
+   "st"            => "good",
+
    "t-initial"     => "wow",
+   "t-final"       => "wow",
+   "t"             => "wow",
+
    "te-final"      => "ok",
-   "th-final"      => "strong",
+   "te-final"      => "ok",
+   "te"            => "ok",
+
    "th-initial"    => "wow",
+   "th-final"      => "strong",
+   "th"            => "wow",
+
    "thr-initial"   => "ok",
+   "thr"           => "ok",
+
    "tr-initial"    => "meh",
+   "tr"            => "meh",
+
    "ts-final"      => "ok",
+   "ts"            => "ok",
+
    "tw-initial"    => "ok",
-   "u-final"       => "good",
+   "tw"            => "ok",
+
    "u-initial"     => "strong",
    "u-middle"      => "strong",
-   "v-final"       => "ok",
+   "u-final"       => "good",
+   "u"             => "strong",
+
    "v-initial"     => "good",
+   "v-final"       => "ok",
+   "v"             => "good",
+
    "ve-final"      => "good",
-   "w-final"       => "strong",
+   "ve"            => "good",
+
    "w-initial"     => "wow",
+   "w-final"       => "strong",
+   "w"             => "wow",
+
    "wh-initial"    => "strong",
+   "wh"            => "strong",
+
    "wn-final"      => "ok",
+   "wn"            => "ok",
+
    "wr-initial"    => "ok",
+   "wr"            => "ok",
+   
    "x-final"       => "good",
-   "y-final"       => "wow",
+   "x"             => "good",
+
    "y-initial"     => "strong",
+   "y-final"       => "wow",
+   "y"             => "wow",
+   
    "yea-initial"   => "meh",
+   "yea"           => "meh",
+   
  # "you-final"     => "strong",
    "you-initial"   => "strong",
+   "you"           => "strong",
 );
 
 
@@ -168,7 +361,7 @@ sub chunk_felicity {
       return 'no';
    }
    
-   # We could speed this up by precalculating these and just putting them in the chunkabet
+   # We could speed this up by precalculating these and just putting them in the chunkabet (2024-05-02 - which I've just done)
    my $s = 'no';
    foreach my $alt (chunk_felicity ($chunk, 'initial'), chunk_felicity ($chunk, 'middle'), chunk_felicity ($chunk, 'final')) {
       $s = $alt if chunk_beats ($alt, $s);
